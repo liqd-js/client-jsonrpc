d05967962c9c149037c8543c13b076ab8cdbfcc2
Tomáš Korenko <radixxko@gmail.com>
2020-01-11 10:15:14 +0100

    Version 0.0.2

---
+++ README.md
@@ -0,0 +1,6 @@
+# @liqd-js/client-jsonrpc
+
+[![NPM version](https://img.shields.io/npm/v/@liqd-js/client-jsonrpc.svg)](https://img.shields.io/npm/v/@liqd-js/client-jsonrpc.svg)
+[![NPM downloads](https://img.shields.io/npm/dm/@liqd-js/client-jsonrpc.svg)](https://img.shields.io/npm/dm/@liqd-js/client-jsonrpc.svg)
+[![Known Vulnerabilities](https://snyk.io/test/github/liqd-js/client-jsonrpc/badge.svg?targetFile=package.json)](https://snyk.io/test/github/liqd-js/client-jsonrpc/badge.svg?targetFile=package.json)
+[![MIT License](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE)
---
+++ test/test.js
@@ -0,0 +1,30 @@
+const { Server } = require('@liqd-js/websocket');
+const Client_JSONRPC = require('../lib/client_jsonrpc.js');
+
+const SLEEP = ( ms ) => new Promise( r => setTimeout( r, ms ));
+const NOOP = () => undefined;
+
+const server = new Server({ port: 8080 });
+
+server.on( 'client', client => 
+{
+    let client_jsonrpc = new Client_JSONRPC( client );
+
+    client_jsonrpc.on( 'call', async( id, method, params ) => 
+    {
+        console.log( 'call', { id, method, params });
+
+        await SLEEP( 1000 );
+
+        client_jsonrpc.result( id, params[0] + params[1] );
+    });
+
+    client.on( 'close', NOOP );
+    client.on( 'error', NOOP );
+});
+
+const client = new Client_JSONRPC('ws://localhost:8080');
+
+client.call( 'sum', 1, 2 ).then( console.log );
+client.call( 'sum', 3, 2 ).then( console.log );
+client.call( 'sum', 4, 3 ).then( console.log );
